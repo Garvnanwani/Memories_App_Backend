171702b3f5d8b97e7bd239f1ded5fd09e51305fa
Garv nanwani <garvnanwani16@gmail.com>
2020-11-02 21:39:48 +0530

    basic server setup

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import cors from "cors"
+import express from "express"
+
+const app = express()
+
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors)
