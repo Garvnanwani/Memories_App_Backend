2c93c812600e5ed3a79e62ffccefd3782f515af4
Garv nanwani <garvnanwani16@gmail.com>
2020-11-02 23:28:00 +0530

    added post route

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import cors from "cors"
-import dotenv from "dotenv"
+import { config } from "dotenv"
 import express from "express"
-import connectDB from "./utils.js/db"
+import postRoutes from "./routes/posts.js"
+import connectDB from "./utils/db.js"
+
+config({ path: "../.env" })
 
 const app = express()
+
+app.use("/posts", postRoutes)
 
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors)
-dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
 connectDB()
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`)
+})
